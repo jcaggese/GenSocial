1ca430a82adcfb485ddbafa909605523082fb079
Elyriand21 <felix.banks02@gmail.com>
2022-06-13 14:32:00 -0400

    Added some CSS for the labels and centered the header text

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Components/Login/Header"
+import Form from "./Components/Login/Form"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
